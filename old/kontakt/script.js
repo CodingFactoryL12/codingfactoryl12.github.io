f414b8755161156be0159cb3f8d975f75736298b
CodingFactoryL12 <129657942+CodingFactoryL12@users.noreply.github.com>
2026-01-09 15:38:17 +0100

    redo

---
+++ old/kontakt/script.js
@@ -0,0 +1,10 @@
+
+
+
+function emailContactButtonClicked(){
+    window.open('mailto:codingfactorylstudios@sfn-kassel.de');
+}
+
+function discordContactButtonClicked(){
+    window.open('https://discord.gg/dKHqgF2y4h');
+}
---
+++ old/script.js
@@ -0,0 +1,19 @@
+function projekteAufrufen(){
+    location.assign('./projekte/index.html');
+}
+
+function kontaktAufrufen(){
+    location.assign('./kontakt/index.html');
+}
+
+function shopAufrufen(){
+    location.assign('./shop/index.html');
+}
+
+function bilder (){
+
+}
+
+
+
+
